65d20a20800f704ae61abd537b339596c6efb00f
Abbas Naderi <git@abiusx.com>
2013-02-12 02:22:28 +0000

    fixed base data and rbac relations

--- install/_db/mysqli.data.sql
+++ install/_db/mysqli.data.sql
@@ -1,20 +1,20 @@
 
 
 INSERT INTO `PREFIX_rbac_permissions` (`ID`, `Left`, `Right`, `Title`, `Description`) VALUES
-(0, 1, 2, 'root', 'root');
+(1, 1, 2, 'root', 'root');
 
 
 INSERT INTO `PREFIX_rbac_rolepermissions` (`RoleID`, `PermissionID`, `AssignmentDate`) VALUES
-(0, 0, 2009);
+(1, 1, 0);
 
 
 
 INSERT INTO `PREFIX_rbac_roles` (`ID`, `Left`, `Right`, `Title`, `Description`) VALUES
-(0, 1, 2, 'root', 'root');
+(1, 1, 2, 'root', 'root');
 
 
 INSERT INTO `PREFIX_rbac_userroles` (`UserID`, `RoleID`, `AssignmentDate`) VALUES
-(1, 0, 2009);
+(1, 1, 0);
 
 INSERT INTO `PREFIX_users` (`ID`, `Username`, `Password`, `Salt`, `Protocol`) VALUES
 (1, 'root', '09095c2a72470b8a203fb4bc5193c548802177b9f786ce1c5c0f423f68ed2518c8ce4c536240fc25be7cae06fc1860f2ec9c3b469770244837b91689c7a0c9a5', 'dff41c6629ca83aee37987d3363ce8c9d17b3c3511797956de9abf3535132d9c2daf1a59a32df97e2adafcbe707c989b9957f44ae336f6dc4ada6ef121eeebf4', 1);
