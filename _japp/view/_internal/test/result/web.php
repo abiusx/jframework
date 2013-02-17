9c69f8dae8720431ed756c8ad56186f24b43faf0
Abbas Naderi <git@abiusx.com>
2013-02-17 16:57:53 +0000

    output is same html, but much more readable when in view source mode

--- _japp/view/_internal/test/result/web.php
+++ _japp/view/_internal/test/result/web.php
@@ -6,31 +6,33 @@
 function DumpResultRows($ResultArray,$BackgroundColor,$Color,$Text,$Odd=false)
 {
 	if (count($ResultArray)):?>
-	<tr style='background-color:<?php exho ($BackgroundColor);?>;color:<?php exho ($Color)?>;font-weight:bold;'>
-	<td colspan='4' >
+<tr style='background-color:<?php exho ($BackgroundColor);?>;color:<?php exho ($Color)?>;font-weight:bold;'>
+<td colspan='4' >
 	<?php exho (count($ResultArray)); 
 	echo " {$Text}";
 	?>
-	</td>
-	<?php 
+	
+</td>
+</tr>
+<?php 
 		$n=0;
 		foreach ($ResultArray as $test)
 		{
 			echo "<tr>";
-			echo "<td width='50' align='center'>";
+			echo "<td width='50' align='center'>\n\t\t";
 			echo ++$n;
-			echo "</td>";
+			echo "\n</td>";
 			$t=$test->failedTest();
-			echo "<td>";
+			echo "<td>\n\t\t";
 			echo get_class($t);
 			echo " :: ";
 			echo $t->getName();
-			echo "</td>";
-			echo "<td>";
+			echo "\n</td>";
+			echo "<td>\n\t\t";
 			$e=new Exception();
  			echo $test->getExceptionAsString();
 			echo "</td>";
-			echo "<td>";
+			echo "<td>\n\t\t";
 			$trace=($test->thrownException()->getTrace());
 			if ($Odd)
 			{
@@ -58,42 +60,37 @@ function DumpResultRows($ResultArray,$BackgroundColor,$Color,$Text,$Odd=false)
 			else
 				echo "File: Could not determine, probably because of fatal error.";
 			
-			echo "</td>";
-			echo "</tr>";
+			echo "\n</td>\n";
+			echo "</tr>\n";
 		}
 		?>
-	</tr>
 	<?php 
 	endif;	
 }
 ?>
-		<h1>Test Results</h1>
-		<table border='1' cellpadding='5' cellspacing='0' width='100%' >
-		<?php if (count ($result->passed())):?>
-		<tr style='background-color:green;color:white;font-weight:bold;'>
-		<td colspan='4' >
-		<?php exho (count($result->passed())); ?> Tests Passed
-		</td>
-		</tr>
-		<?php
-		endif;
-		DumpResultRows($result->failures(), "red", "white", "Tests Failed");
-		DumpResultRows($result->errors(), "#FF7700", "white", "Tests Have Errors");
-		DumpResultRows($result->notImplemented(), "yellow", "blue", "Tests Not Implemented",true);
-		DumpResultRows($result->skipped(), "gray", "white", "Tests Skipped",true);
-		?>
+<h1>Test Results</h1>
 
-		<tr style='background-color:black;color:white;text-align:right;'>
-		<td colspan='4'>
-		<span style='float:left;'>
-		Time: <?php echo $profiler->Timer()?> seconds
-		</span> 
-		Total: <?php echo ($result->count());?> Tests 
-		in
-		<?php echo count(\jf\TestLauncher::$TestFiles);?> Files
-		</td>
-		</tr>
-		
+<table border='1' cellpadding='5' cellspacing='0' width='100%' >
+<?php if (count ($result->passed())):?>
+<tr style='background-color:green;color:white;font-weight:bold;'>
+<td colspan='4' >
+	<?php exho (count($result->passed())); ?> Tests Passed
+</td>
+</tr>
+<?php
+endif;
+DumpResultRows($result->failures(), "red", "white", "Tests Failed");
+DumpResultRows($result->errors(), "#FF7700", "white", "Tests Have Errors");
+DumpResultRows($result->notImplemented(), "yellow", "blue", "Tests Not Implemented",true);
+DumpResultRows($result->skipped(), "gray", "white", "Tests Skipped",true);
+?>
+
+<tr style='background-color:black;color:white;text-align:right;'>
+<td colspan='4'>
+	<span style='float:left;'>Time: <?php echo $profiler->Timer()?> seconds</span> Total: <?php echo ($result->count());?> Tests in <?php echo count(\jf\TestLauncher::$TestFiles);?> Files
+</td>
+</tr>
+
 		
 		
 		
