a868a0cacd719fdf24f8994cddff21a2bf86352b
Saeed Mortazavi <s.mortazavi.1991@gmail.com>
2013-04-20 13:15:26 +0000

    test of mariadb debuged: using prepareCache and restoring DefaultIndex of DatabaseManager

--- _japp/test/lib/db/adapter/mariadb.php
+++ _japp/test/lib/db/adapter/mariadb.php
@@ -2,24 +2,28 @@
 jf::import("jf/test/lib/db/adapter/pdo_mysql");
 class LibDbMariadbTest extends LibDbPdoMysqlTest
 {
+	private static $Default;
 	public static function setUpBeforeClass()
 	{
 		parent::setUpBeforeClass();
+		self::$Default=\jf\DatabaseManager::$DefaultIndex;
 		$setting=\jf\DatabaseManager::Configuration();
 		$config=new \jf\DatabaseSetting("mariaDB", $setting->DatabaseName, $setting->Username, $setting->Password, $setting->Host, $setting->TablePrefix);
 		\jf\DatabaseManager::AddConnection($config,2);
-		\jf\DatabaseManager::$DefaultIndex=2;
+		\jf\DatabaseManager::$DefaultIndex=\jf\DatabaseManager::FindIndex($config);
 	}
 }
 
-class LibJfDbalMariadbStatementTest extends LibJfDbalPdoMysqlStatementTest
+class LibDbStatementMariadbTest extends LibDbStatementPdoMysqlTest
 {
+	private static $Default;
 	public static function setUpBeforeClass()
 	{
 		parent::setUpBeforeClass();
+		self::$Default=\jf\DatabaseManager::$DefaultIndex;
 		$setting=\jf\DatabaseManager::Configuration();
 		$config=new \jf\DatabaseSetting("mariaDB", $setting->DatabaseName, $setting->Username, $setting->Password, $setting->Host, $setting->TablePrefix);
 		\jf\DatabaseManager::AddConnection($config,2);
-		\jf\DatabaseManager::$DefaultIndex=2;
+		\jf\DatabaseManager::$DefaultIndex=\jf\DatabaseManager::FindIndex($config);
 	}
 }
