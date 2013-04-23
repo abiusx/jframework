d3d113f8ea4b21ba6fa694f9df354d270fc169d8
Abbas Naderi <git@abiusx.com>
2013-04-23 07:50:18 +0000

    added jquery

--- app/view/default/_template/head.php
+++ app/view/default/_template/head.php
@@ -6,6 +6,7 @@
 <title><?php echo $this->Title()?:jf_Application_Title; ?></title>
 <link rel="shortcut icon" href="<?php echo (jf::url());?>/img/jlogo.png" />
 <link rel="stylesheet" href="<?php echo (jf::url());?>/style/base.css" />
+<script src="<?php echo jf::url();?>/script/jquery-1.9.1.min.js"></script>
 <?php echo $this->HeadData();?>
 <base href="<?php echo jf::url();?>" />
 </head>
